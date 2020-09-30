a3e319c03a3f0451dc73e2a6c9af189636d4c295
Manav Misra <manavm@visionwebsoft.com>
2020-09-30 11:54:11 -0500

    :wrench: Configure eslint to use 'base airbnb'

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb", "prettier"],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
