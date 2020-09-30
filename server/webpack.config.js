6b018b7fff021553c9c87c6b774e283df8133b5d
Manav Misra <manavm@visionwebsoft.com>
2020-09-30 12:46:24 -0500

    :heavy_plus_sign: Add 'babel' for webpack
    :wrench: Configure webpack to use 'babel'

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,6 +2,17 @@ module.exports = {
   mode: "development",
   target: "node",
   module: {
-    rules: [{ test: /\.graphql?$/, loader: "webpack-graphql-loader" }],
+    rules: [
+      { test: /\.graphql?$/, loader: "webpack-graphql-loader" },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
