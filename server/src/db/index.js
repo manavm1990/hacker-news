c7646a3cd76f75aef94d7fbe2e2c6b35073f0b65
Manav Misra <manavm@visionwebsoft.com>
2020-10-01 10:25:08 -0500

    :seedling: Add starter 'db' file to use 'prisma'
    https://www.howtographql.com/graphql-js/4-adding-a-database/

---
+++ server/src/db/index.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+(async () => {
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+})()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
