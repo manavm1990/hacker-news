834df26be66052272dfdc42c1b9ef6739cf61bd8
Manav Misra <manavm@visionwebsoft.com>
2020-10-02 11:05:04 -0500

    :recycle: Refactor for implicit return on arrow ➡️ fxn

--- server/src/graphql/resolvers/feed.resolvers.js
+++ server/src/graphql/resolvers/feed.resolvers.js
@@ -6,10 +6,8 @@ module.exports = {
   },
 
   Mutation: {
-    post: (_, { url, description }, { prisma }) => {
-      const newLink = prisma.link.create({ data: { url, description } });
-      return newLink;
-    },
+    post: (_, { url, description }, { prisma }) =>
+      prisma.link.create({ data: { url, description } }),
     update: (_, { id, description, url }, { prisma }) =>
       prisma.link.update({ data: { description, url }, where: { id } }),
     delete: (_, { id }, { prisma }) => prisma.link.delete({ where: { id } }),
