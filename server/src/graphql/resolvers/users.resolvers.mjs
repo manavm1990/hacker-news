eb8806e880b500bd143a49294a0ebf339e9e164b
Manav Misra <manavm@visionwebsoft.com>
2020-10-02 15:21:43 -0500

    :construction: Add 'User resolvers'

---
+++ server/src/graphql/resolvers/users.resolvers.mjs
@@ -0,0 +1,35 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  Query: {
+    login: async (_, { email, password }, { prisma }) => {
+      const user = await prisma.user.findOne({ where: { email } });
+      if (!user) {
+        throw new Error("🙅🏽‍♂️ No such user found!");
+      }
+
+      const validated = await bcrypt.compare(password, user.password);
+      if (!validated) {
+        throw new Error("🙅🏽‍♂️ Invalid password!");
+      }
+
+      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+      return { token, user };
+    },
+  },
+
+  Mutation: {
+    signup: async (_, args, { prisma }) => {
+      const hashedPass = await bcrypt.hash(args.password, 10);
+      const newUser = prisma.user.create({ data: { ...args, hashedPass } });
+      const token = jwt.sign({ userId: newUser.id }, process.env.APP_SECRET);
+
+      return { token, newUser };
+    },
+  },
+};
