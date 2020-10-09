628997a91cea836af11b81d5e44fbcf3e1a5d760
Manav Misra <manavm@visionwebsoft.com>
2020-10-09 09:15:52 -0500

    :fire: Remove irrelevant comment

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,6 +1,5 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 
-// TODO: Figure out how to use 'loadFilesSync'
 import feedResolver from "./feed.resolver";
 import infoResolver from "./info.resolver";
 import usersResolver from "./user.resolver";
